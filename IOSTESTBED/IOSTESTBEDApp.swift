92a1ed9c598f8c8b918a3c2457a917806ee783e5
KONG <oojun82@gmail.com>
2023-12-23 00:48:06 +0900

    test

---
+++ IOSTESTBED/IOSTESTBEDApp.swift
@@ -0,0 +1,20 @@
+//
+//  IOSTESTBEDApp.swift
+//  IOSTESTBED
+//
+//  Created by KX60 on 2023/12/23.
+//
+
+import SwiftUI
+
+@main
+struct IOSTESTBEDApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
